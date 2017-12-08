1913f04ec462f20d8dbe94b0675eb27a66c97ce5
JoepStraatman <straatmanjoep@gmail.com>
2017-12-08 11:14:09 +0100

    better code hub improvement

--- app/src/main/java/com/example/joeps/triviaplaza/Main.java
+++ app/src/main/java/com/example/joeps/triviaplaza/Main.java
@@ -1,13 +1,45 @@
 package com.example.joeps.triviaplaza;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Main extends AppCompatActivity {
+    private FirebaseAuth authTest;
+    private FirebaseAuth.AuthStateListener authListenerTest;
+    private static final String Tag = "Firebase_test";
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        authTest = FirebaseAuth.getInstance();
+        setListener();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+    private void setListener(){
+        authListenerTest = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Log.d(Tag, "onAuthStateChanged:signed_in"+user.getUid());
+                }else{
+                    Log.d(Tag, "onAuthStateChanged:signed_out");
+                    goToHome();
+                }
+            }
+        };
+    }
+    private void goToHome() {
+        startActivity(new Intent(getApplicationContext(), Home_screen.class));finish();
     }
 }
